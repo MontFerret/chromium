92e632c595e8c22d070297df88fa456a2f3b81e3
Tim Voronov <ziflex@gmail.com>
2020-09-16 22:05:23 -0400

    Removed --single-process flag

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,14 +5,18 @@ set -e
 CHROME_ARGS="--disable-background-networking \
 --disable-background-timer-throttling \
 --disable-breakpad \
+--disable-canvas-aa \
 --disable-client-side-phishing-detection \
 --disable-cloud-import \
+--disable-composited-antialiasing \
 --disable-default-apps \
 --disable-demo-mode \
 --disable-dev-shm-usage \
 --disable-extensions \
+--disable-hang-monitor \
 --disable-gesture-typing \
 --disable-gpu \
+--disable-gpu-sandbox \
 --disable-infobars \
 --disable-kill-after-bad-ipc \
 --disable-notifications \
@@ -23,6 +27,9 @@ CHROME_ARGS="--disable-background-networking \
 --disable-print-preview \
 --disable-prompt-on-repost \
 --disable-renderer-backgrounding \
+--disable-seccomp-filter-sandbox \
+--disable-setuid-sandbox \
+--disable-smooth-scrolling \
 --disable-speech-api \
 --disable-sync \
 --disable-tab-for-desktop-share \
@@ -30,6 +37,8 @@ CHROME_ARGS="--disable-background-networking \
 --disable-voice-input \
 --disable-wake-on-wifi \
 --disable-web-security \
+--disk-cache-dir=/tmp/cache-dir \
+--disk-cache-size=10000000 \
 --enable-async-dns \
 --enable-simple-cache-backend \
 --enable-tcp-fast-open \
@@ -42,9 +51,11 @@ CHROME_ARGS="--disable-background-networking \
 --ignore-gpu-blocklist \
 --ignore-ssl-errors \
 --log-level=0 \
+--media-cache-size=10000000 \
 --metrics-recording-only \
 --mute-audio \
 --no-default-browser-check \
+--no-experiments \
 --no-first-run \
 --no-pings \
 --no-sandbox \
@@ -53,7 +64,6 @@ CHROME_ARGS="--disable-background-networking \
 --remote-debugging-address=$DEBUG_ADDRESS \
 --remote-debugging-port=$DEBUG_PORT \
 --safebrowsing-disable-auto-update \
---single-process \
 --use-gl=swiftshader \
 --user-data-dir=$HOME"
 
